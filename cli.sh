a4e9a9dcc3266bd83f771751e8472613e3ca6f8a
ErikAllanKincaid <erikallankincaid@gmail.com>
2021-05-21 22:14:13 -0700

    git origin howto

--- cli.sh
+++ cli.sh
@@ -9879,6 +9879,13 @@ $> firefox https://www.fmald.net/
 ## rembg removes backgrounds from images
 $> firefox https://github.com/danielgatis/rembg
 ##==========================================
+## Change Git Remote URL
+## In order to change the URL of a Git remote, you have to use the “git remote set-url” command and specify the name of the remote as well as the new remote URL to be changed.
+$> git remote set-url <remote_name> <remote_url>
+## For example, let’s say that you want to change the URL of your Git origin remote.
+## In order to achieve that, you would use the “set-url” command on the “origin” remote and you would specify the new URL.
+$> git remote set-url origin git@github.com:ErikAllanKincaid/LinuxCommands.git
+##==========================================
 
 
 ##==========================================
