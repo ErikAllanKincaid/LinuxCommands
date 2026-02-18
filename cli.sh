9998ccd2b1bd7d5007f61d4edd83a106a0ba7931
Erik Kincaid <erikallankincaid@gmail.com>
2026-02-18 12:34:36 -0800

    Change start for options.

--- cli.sh
+++ cli.sh
@@ -1216,34 +1216,34 @@ $> while :; do clr;  git --no-pager log -33 --graph --all --date=short --pretty=
 ## Git log
 ## Useful specifiers for git log --pretty=format
 ## Specifier       Description of Output
-$>  %H              ## Commit hash
-$>  %h              ## Abbreviated commit hash
-$>  %T              ## Tree hash
-$>  %t              ## Abbreviated tree hash
-$>  %P              ## Parent hashes
-$>  %p              ## Abbreviated parent hashes
-$>  %an             ## Author name
-$>  %ae             ## Author email
-$>  %ad             ## Author date (format respects the --date=option)
-$>  %ar             ## Author date, relative
-$>  %cn             ## Committer name
-$>  %ce             ## Committer email
-$>  %cd             ## Committer date
-$>  %cr             ## Committer date, relative
-$>  %s              ## Subject
+#>  %H              ## Commit hash
+#>  %h              ## Abbreviated commit hash
+#>  %T              ## Tree hash
+#>  %t              ## Abbreviated tree hash
+#>  %P              ## Parent hashes
+#>  %p              ## Abbreviated parent hashes
+#>  %an             ## Author name
+#>  %ae             ## Author email
+#>  %ad             ## Author date (format respects the --date=option)
+#>  %ar             ## Author date, relative
+#>  %cn             ## Committer name
+#>  %ce             ## Committer email
+#>  %cd             ## Committer date
+#>  %cr             ## Committer date, relative
+#>  %s              ## Subject
 ## Example
 $> git log --pretty=format:"%h %s" --graph
 ## Option              Description
-$>  -p                  ## Show the patch introduced with each commit.
-$>  --stat              ## Show statistics for files modified in each commit.
-$>  --shortstat         ## Display only the changed/insertions/deletions line from the --stat command.
-$>  --name-only         ## Show the list of files modified after the commit information.
-$>  --name-status       ## Show the list of files affected with added/modified/deleted information as well.
-$>  --abbrev-commit     ## Show only the first few characters of the SHA-1 checksum instead of all 40.
-$>  --relative-date     ## Display the date in a relative format (for example, “2 weeks ago”) instead of using the full date format.
-$>  --graph             ## Display an ASCII graph of the branch and merge history beside the log output.
-$>  --pretty            ## Show commits in an alternate format. Option values include oneline, short, full, fuller, and format (where you specify your own format).
-$>  --oneline           ## Shorthand for --pretty=oneline --abbrev-commit used together.
+#>  -p                  ## Show the patch introduced with each commit.
+#>  --stat              ## Show statistics for files modified in each commit.
+#>  --shortstat         ## Display only the changed/insertions/deletions line from the --stat command.
+#>  --name-only         ## Show the list of files modified after the commit information.
+#>  --name-status       ## Show the list of files affected with added/modified/deleted information as well.
+#>  --abbrev-commit     ## Show only the first few characters of the SHA-1 checksum instead of all 40.
+#>  --relative-date     ## Display the date in a relative format (for example, “2 weeks ago”) instead of using the full date format.
+#>  --graph             ## Display an ASCII graph of the branch and merge history beside the log output.
+#>  --pretty            ## Show commits in an alternate format. Option values include oneline, short, full, fuller, and format (where you specify your own format).
+#>  --oneline           ## Shorthand for --pretty=oneline --abbrev-commit used together.
 ##==========================================
 ## Correct an unpushed commit message
 $> git commit --amend -m "New message"
